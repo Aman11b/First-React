78e345644887a9b9c51ad55d15ae43c3eb831744
Aman11b <amansingh1100b@gmail.com>
2025-01-18 17:18:44 +0530

    prented hello world

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-
+function App(){
+  return(
+    <>
+      <h1>Hello there</h1>
+      <p>This is my first react app</p>
+    </>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <>
-    <h1>Hello there</h1>
-    <p>This is my first react app</p>
+    <App />
   </>
 )
